ff6c80170b552042ab85fff9e7ea846c718002a0
Pedro Goncalves <pmogs@iscte-iul.pt>
2017-03-18 15:24:13 +0000

    fd

--- src/main/java/mt/filter/AnalyticsFilter.java
+++ src/main/java/mt/filter/AnalyticsFilter.java
@@ -10,7 +10,7 @@ import mt.comm.ServerSideMessage;
 import mt.comm.ServerSideMessage.Type;
 
 public class AnalyticsFilter implements ServerComm {
-//nothing to do here!
+//nothing to do here!cx
 	private ServerComm decoratedServerComm;
 	private int buyOrders = 0;
 	private int sellOrders = 0;
